ccda4ed87a295624c553184af395f87e72bf5113
PavolGrofcik <grofcik.pavol@gmail.com>
2025-03-08 21:18:06 +0100

    Functional Postgres database

---
+++ compose/init.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+echo "##############################"
+echo "Initialazation starting..."
+echo "##############################"
+
+pip3 install pyarrow 
+python3 -m pip install "dask[distributed]"
+apt update -y
+
+#airflow db init
+#airflow users create --username admin --password admin --firstname test  --lastname test --role Admin --email test@test.org
+
+mkdir /root/airflow/dags
+cp ./dags/* /root/airflow/dags/
+
+echo "##############################"
+echo "Initialization successful!"
+echo "##############################"
